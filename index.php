06a2aa78c49f76c1047a31d4c3e1c3b4889c6491
Rafael Maciel <rafaelmaciels@outlook.com>
2026-02-05 05:29:11 -0300

    Primeira Versão  do meu Site
    A primeira versão do meu site ficou rodando de 2016 a 2026, quando resolvi criar um portfólio.

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-53879063-1"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'UA-53879063-1');
+    </script>
+    <meta charset="UTF-8">
+    <title>Rafael Maciel - WebDeveloper</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+
+<body>
+<?php include_once("analyticstracking.php") ?>
+<div id="wrap">
+    <span>rafael@webdev:~$ </span>
+</div>
+</body>
+</html>
